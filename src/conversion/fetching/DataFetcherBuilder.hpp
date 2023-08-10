643ceaa1a5add5feecc230eb93796d26e91e7d96
Mateusz <mwojciechowski296@gmail.com>
2023-08-10 22:43:16 +0200

    Update

--- src/conversion/fetching/DataFetcherBuilder.hpp
+++ src/conversion/fetching/DataFetcherBuilder.hpp
@@ -8,9 +8,19 @@ template<class SizeT>
 class DataFetcherBuilder {
     DataFetcherConfig<SizeT> config {};
 
-    void setHostsContainer(std::vector<Host<SizeT>>* hosts);
-    void setRangesContainer();
-    void setSubnetsContainer();
+    void setHostsContainer(std::vector<Host<SizeT>>* hosts) {
+        config.hosts = hosts;
+    }
 
-    void setRecordsDelimiter();
+    void setRangesContainer(std::vector<Range<SizeT>>* ranges) {
+        config.ranges = ranges;
+    }
+
+    void setSubnetsContainer(std::vector<Subnet<SizeT>>* subnets) {
+        config.subnets = subnets;
+    }
+
+    void setRecordsDelimiter(const char* delimiter) {
+        config.recordsDelimiter = delimiter;
+    }
 };
